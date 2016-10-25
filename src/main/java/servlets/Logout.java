8131c24babee6098053bbcf8cf2a92c5dce5fb6a
alinkin9696 <alinkin9696@mail.ru>
2016-10-25 15:16:27 +0300

    add logout function

---
+++ src/main/java/servlets/Logout.java
@@ -0,0 +1,21 @@
+package servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by alina on 25.10.16.
+ */
+public class Logout extends HttpServlet {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getSession().invalidate();
+        resp.sendRedirect("/mainPage");
+    }
+}
