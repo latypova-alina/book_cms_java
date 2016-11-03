46ef122c87f64fb4c3be339a92a50e8d685da516
alinkin9696 <alinkin9696@mail.ru>
2016-11-03 22:41:52 +0300

    add show film servise and servlet

---
+++ src/main/java/servlets/ShowFilms.java
@@ -0,0 +1,35 @@
+package servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static Services.FilmImageService.searchFilmImage;
+
+/**
+ * Created by alina on 02.11.16.
+ */
+public class ShowFilms extends HttpServlet{
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        int film_id = 1;
+
+        if (searchFilmImage(film_id) != null) {
+
+            String image_path = searchFilmImage(film_id);
+
+
+
+            resp.setContentType("text/plain");  // Set content type of the response so that jQuery knows what it can expect.
+            resp.setCharacterEncoding("UTF-8"); // You want world domination, huh?
+            resp.getWriter().write(image_path);
+
+
+        }
+
+
+
+    }
+}
