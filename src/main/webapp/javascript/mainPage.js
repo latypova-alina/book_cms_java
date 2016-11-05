2d96eda049eb4a8f247f848e07e42ed0d9834c9c
alinkin9696 <alinkin9696@mail.ru>
2016-11-05 23:47:28 +0300

    add new functions to display names and descriptions of actors and films

--- src/main/webapp/javascript/mainPage.js
+++ src/main/webapp/javascript/mainPage.js
@@ -11,14 +11,49 @@ $(document).ready(function(){
                 img.src = image_path[i - 1];
             }
         });
+        $.get("/showActorsNames", function (responseText) {
+            var paths = responseText;
+            paths = paths.replace('[','');
+            paths = paths.replace(']','');
+            var names = paths.split(", ");
+            for (i = 1; i<3; i++) {
+                var name_title = document.getElementById('name_' + i);
+                name_title.innerText = names[i - 1];
+            }
+        });
 
     }
     function showFilms() {
-        $.get("/showFilms", function(responseText) {
-            var image_path = responseText;
-            img.src = image_path;
+        $.get("/showFilms", function (responseText) {
+            var paths = responseText;
+            paths = paths.replace('[','');
+            paths = paths.replace(']','');
+            var image_path = paths.split(", ");
+            for (i = 1; i<3; i++) {
+                var img = document.getElementById('image_' + i);
+                img.src = image_path[i - 1];
+            }
         });
-        image_id = image_id + 1;
+        $.get("/showFilmsNames", function (responseText) {
+            var paths = responseText;
+            paths = paths.replace('[','');
+            paths = paths.replace(']','');
+            var names = paths.split(", ");
+            for (i = 1; i<3; i++) {
+                var name_title = document.getElementById('name_' + i);
+                name_title.innerText = names[i - 1];
+            }
+        });
+        $.get("/showFilmsDescription", function (responseText) {
+            var paths = responseText;
+            paths = paths.replace('[','');
+            paths = paths.replace(']','');
+            var description = paths.split("TheEndOfDescription, ");
+            for (i = 1; i<3; i++) {
+                var name_title = document.getElementById('description_' + i);
+                name_title.innerText = description[i - 1];
+            }
+        });
     }
     document.getElementById('actors').onclick = showActors;
     document.getElementById('films').onclick = showFilms;
